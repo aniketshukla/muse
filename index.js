8a69d6999f5c8b6450d58ea2df735e26388ebd07
aniketshukla <asr563up@gmail.com>
2017-08-27 10:28:38 +0530

    Adder error handler and loggin capabilities

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ var restify = require('restify');
 var UserRoutes = require('./routes/user.server.routes');
 var jsonPatch = require('./routes/patch.server.routes');
 var ImageRoutes = require('./routes/image.server.routes');
+var logger = require('./logger/bunyan_logger');
 
 /**
 Declaring server properties
 */
 const server = restify.createServer({
   name: 'muse',
-  version: '0.0.1'
+  version: '0.0.1',
+  log: logger
 });
 
 server.use(restify.plugins.acceptParser(server.acceptable));
@@ -28,6 +30,13 @@ ImageRoutes(server);
 server.get(/\/public\/.*/, restify.plugins.serveStatic({
   directory: __dirname
 }));
+
+server.on('restifyError', function (req, res, err, cb) {
+  // this listener will fire after both events above!
+  // `err` here is the same as the error that was passed to the above
+  // error handlers.
+  server.log.error({req: req, res: res, err: err});
+});
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
